aff3dcfaf289316973eca25f4b78a5b4625637c1
RevanthR <revanthrobbie37@gmail.com>
2019-03-09 13:26:09 +0530

    Changes made to the chatbot

--- chatbot.py
+++ chatbot.py
@@ -2,8 +2,7 @@ import nltk
 import numpy as np
 import random
 import string
-from sklearn.feature_extraction.text import TfidVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
+
 
 f=open('chatbot.txt','r',errors='ignore')
 raw=f.read()
@@ -18,18 +17,19 @@ word_tokens=nltk.word_tokenize(raw)# convert to list of words
 
 #normalise the tokens
 
-lemmer = nltk.stem.WordNetLemmetizer()
+lemmer = nltk.stem.WordNetLemmatizer()
 
 def LemTokens(tokens):
 	return [lemmer.lemmatize(token) for token in tokens]
 remove_punct_dict = dict((ord(punct),None) for punct in string.punctuation)
 
 def LemNormalize(text):
-	return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict))
-
+	return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 #Keyword Matching
 
-GREETING_INPUTS = ("hello","hi","greetings","sup","what's up","hey",)
+GREETING_INPUTS= ("hello","hi","greetings","sup","what's up","hey",)
 
 GREETING_RESPONSES = ["Hi","Hey","*nods*","Hi there","Hello","I am glad! You are talking to me"]
 
@@ -37,11 +37,19 @@ def greeting(sentence):
 	for word in sentence.split():
 		if word.lower() in GREETING_INPUTS:
 			return random.choice(GREETING_RESPONSES)
+name_input=("your name")
+name_response=["My Name is ROBO"]
+
+def name(sentence):
+	for word in sentence.split():
+		if word.lower() in name_input:
+			return name_response
+
 def response(user_response):
-	robo_reponse=''
+	robo_reponse=""
 	sent_tokens.append(user_response)
 
-	TfidVec= TfidVectorizer(tokenizer = LemNormalize, stop_words='english')
+	TfidVec= TfidfVectorizer(tokenizer = LemNormalize, stop_words='english')
 	tfidf= TfidVec.fit_transform(sent_tokens)
 	vals = cosine_similarity(tfidf[-1],tfidf)
 	idx= vals.argsort() [0][-2]
@@ -50,19 +58,20 @@ def response(user_response):
 	req_tfidf= flat[-2]
 
 	if(req_tfidf==0):
-		robo_response =robo_response + "I am sorry! I dont understand you"
+		robo_response="I am sorry! I dont understand you"
 		return robo_response
 	else:
 		robo_response = robo_reponse+sent_tokens[idx]
 		return robo_response
 flag=True
 print("ROBO : My name is Robo. I will always answer to your queries.")
-
-while(flag=True):
+name=['whats your name','what is your name','your name']
+while(flag==True):
+	print("Me:",end=" ")
 	user_response=input()
 	user_response=user_response.lower()
 	if(user_response!='bye'):
-		if(user_response=='thanks' or user_response='thank you'):
+		if(user_response=='thanks' or user_response=='thank you'):
 			flag=False
 			print("ROBO:"+greeting(user_response))
 		else:
@@ -72,6 +81,8 @@ while(flag=True):
 				print("ROBO:",end="")
 				print(response(user_response))
 				sent_tokens.remove(user_response)
+	elif(user_response.lower()=='whats your name'):
+		print("My name is Robo")
 	else:
 		flag=False
 		print("ROBO: Bye! Take Care..")
